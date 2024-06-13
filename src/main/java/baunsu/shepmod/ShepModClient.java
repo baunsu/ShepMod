e702856fec9f6e6963a2e6f6d4c2d284a9aa62ad
Baunsu <zdixon008@gmail.com>
2024-06-12 21:59:46 -0700

    ShepMod Created

---
+++ src/main/java/baunsu/shepmod/ShepModClient.java
@@ -0,0 +1,10 @@
+package baunsu.shepmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShepModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/baunsu/shepmod/ShepModDataGenerator.java
@@ -0,0 +1,11 @@
+package baunsu.shepmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ShepModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
